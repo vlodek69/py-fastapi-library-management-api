0e859d36bd5bcf945516144177d5d999d2c42cba
Vlad Larin <lordgadyka71@gmail.com>
2024-05-01 20:44:27 +0300

    add create-endpoints

--- library/main.py
+++ library/main.py
@@ -27,6 +27,16 @@ def read_authors(db: Session = Depends(get_db)):
     return crud.get_authors(db=db)
 
 
+@app.post("/authors/", response_model=schemas.Author)
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+    return crud.create_author(db=db, author=author)
+
+
 @app.get("/books/", response_model=list[schemas.Book])
-def read_authors(db: Session = Depends(get_db)):
+def read_books(db: Session = Depends(get_db)):
     return crud.get_books(db=db)
+
+
+@app.post("/books/", response_model=schemas.Book)
+def create_books(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    return crud.create_book(db=db, book=book)
