0dd5ed98633ad6f78779a0ecedc675c53e7527b3
Vlad Larin <lordgadyka71@gmail.com>
2024-05-01 19:52:26 +0300

    add create and list functions for Author and Book models

---
+++ library/crud.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+
+from library import models, schemas
+
+
+def get_authors(db: Session):
+    return db.query(models.Author).all()
+
+
+def create_author(db: Session, author: schemas.AuthorCreate):
+    db_author = models.Author(**author.dict())
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+
+    return db_author
+
+
+def get_books(db: Session):
+    return db.query(models.Book).all()
+
+
+def create_book(db: Session, book: schemas.BookCreate):
+    db_book = models.Author(**book.dict())
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+
+    return db_book
