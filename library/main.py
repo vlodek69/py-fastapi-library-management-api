a2da0a1f573a2e2c1ea0ab7c49aab838b825cc1b
Vlad Larin <lordgadyka71@gmail.com>
2024-05-01 20:03:13 +0300

    add get_db, read_authors and read_authors endpoints to main.py

--- library/main.py
+++ library/main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from library import models, schemas, crud
+from library.database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -8,6 +22,11 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/authors/", response_model=list[schemas.Author])
+def read_authors(db: Session = Depends(get_db)):
+    return crud.get_authors(db=db)
+
+
+@app.get("/books/", response_model=list[schemas.Book])
+def read_authors(db: Session = Depends(get_db)):
+    return crud.get_books(db=db)
