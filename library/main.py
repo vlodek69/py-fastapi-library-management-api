27f22c5b111a0b7f2814e33b584f951c852a3717
Vlad Larin <lordgadyka71@gmail.com>
2024-05-04 19:32:27 +0300

    add read_author in main.py

--- library/main.py
+++ library/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from library import models, schemas, crud
@@ -24,6 +24,14 @@ def read_root():
 @app.get("/authors/", response_model=list[schemas.Author])
 def read_authors(db: Session = Depends(get_db)):
     return crud.get_authors(db=db)
+
+
+@app.get("/authors/{author_id}", response_model=schemas.Author)
+def read_author(author_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_author_by_id(db=db, author_id=author_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return db_user
 
 
 @app.post("/authors/", response_model=schemas.Author)
