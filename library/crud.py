006727b985967e02cea52f251bfdad1072eff10c
Vlad Larin <lordgadyka71@gmail.com>
2024-05-04 20:19:07 +0300

    implement books filtering by author_id

--- library/crud.py
+++ library/crud.py
@@ -8,7 +8,9 @@ def get_authors(db: Session, skip: int = 0, limit: int = 2):
 
 
 def get_author_by_id(db: Session, author_id: int):
-    return db.query(models.Author).filter(models.Author.id == author_id).first()
+    return (
+        db.query(models.Author).filter(models.Author.id == author_id).first()
+    )
 
 
 def create_author(db: Session, author: schemas.AuthorCreate):
@@ -20,8 +22,17 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_books(db: Session, skip: int = 0, limit: int = 2):
-    return db.query(models.Book).offset(skip).limit(limit).all()
+def get_books(
+    db: Session, skip: int = 0, limit: int = 2, author_id: int = None
+):
+    queryset = db.query(models.Book)
+
+    if author_id is not None:
+        queryset = queryset.filter(models.Book.author_id == author_id)
+
+    queryset = queryset.offset(skip).limit(limit)
+
+    return queryset.all()
 
 
 def create_book(db: Session, book: schemas.BookCreate):
--- library/main.py
+++ library/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from library import models, schemas, crud
-from library.database import engine, SessionLocal
+from library import schemas, crud
+from library.database import SessionLocal
 
 
 app = FastAPI()
@@ -22,19 +22,12 @@ def read_root():
 
 
 @app.get("/authors/", response_model=list[schemas.Author])
-def read_authors(
-    skip: int = 0,
-    limit: int = 2,
-    db: Session = Depends(get_db)
-):
+def read_authors(skip: int = 0, limit: int = 2, db: Session = Depends(get_db)):
     return crud.get_authors(db=db, skip=skip, limit=limit)
 
 
 @app.get("/authors/{author_id}", response_model=schemas.Author)
-def read_author(
-    author_id: int,
-    db: Session = Depends(get_db)
-):
+def read_author(author_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_author_by_id(db=db, author_id=author_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="Author not found")
@@ -42,10 +35,7 @@ def read_author(
 
 
 @app.post("/authors/", response_model=schemas.Author)
-def create_author(
-    author: schemas.AuthorCreate,
-    db: Session = Depends(get_db)
-):
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
     return crud.create_author(db=db, author=author)
 
 
@@ -53,9 +43,10 @@ def create_author(
 def read_books(
     skip: int = 0,
     limit: int = 2,
-    db: Session = Depends(get_db)
+    author_id: int = None,
+    db: Session = Depends(get_db),
 ):
-    return crud.get_books(db=db, skip=skip, limit=limit)
+    return crud.get_books(db=db, skip=skip, limit=limit, author_id=author_id)
 
 
 @app.post("/books/", response_model=schemas.Book)
